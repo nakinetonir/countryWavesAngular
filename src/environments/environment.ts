df5d1f2e7a35ff49368c14e7fa050ccabed0911a
nakinetonir <nachorio82@gmail.com>
2020-10-04 22:46:58 +0200

    s

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  endpoint : 'http://ima.pythonanywhere.com/',
+  endpoint : 'https://ima.pythonanywhere.com/',
   postDataCountry : '/postDataCountry'
 };
 
